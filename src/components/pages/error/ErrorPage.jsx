9ce1b279d8721f410d72a3eef57c93b85f4b5534
Zer0 <Zer0_AbS0luT@proton.me>
2023-12-07 01:04:04 +0100

    ✨ F02: ORDER > Add redirection from/to ErrorPage

--- src/components/pages/error/ErrorPage.jsx
+++ src/components/pages/error/ErrorPage.jsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
     return (
-        <div>ErrorPage</div>
+        <div>
+            <h1>ErrorPage</h1>
+            <br />
+            <Link to="/"><button>Retourner vers la page d'accueil</button></Link>
+        </div>
     )
 }
