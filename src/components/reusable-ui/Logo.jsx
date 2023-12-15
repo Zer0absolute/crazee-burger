e1701f961fab8f216f1237bb6ba1546461add333
Zer0 <Zer0_AbS0luT@proton.me>
2023-12-15 01:25:43 +0100

    💄 F03: LOGIN > Update Logo.jsx style

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo() {
         <LogoStyled>
             <h1>CRAZEE</h1>
             <div className="img-container">
-                <img src="public/images/F03 logo-burger-orange.png" alt="Logo Burger" />
+                <img src="/public/images/F03 logo-burger-orange.png" alt="Logo Burger" />
             </div>
             <h1>BURGER</h1>
         </LogoStyled>
@@ -23,6 +23,7 @@ const LogoStyled = styled.div`
         font-size: 6.875rem;
         letter-spacing: 0.0938rem;
         text-transform: uppercase;
+        margin: 0;
     }
 
     .img-container img{ 
