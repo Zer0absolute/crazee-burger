55ef77a3bf7229b1855c9ddb321f3cbde4020db7
Zer0 <Zer0_AbS0luT@proton.me>
2023-12-29 18:03:38 +0100

    💄 F05: MAIN > MENU > CARD > Add theme design and finally layout

--- src/components/pages/order/main/Card.jsx
+++ src/components/pages/order/main/Card.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import PrimaryButton from "../../../reusable-ui/PrimaryButton"
+import { theme } from "../../../../theme";
 
 export default function Card({ title, imageSource, leftDescription }) {
     return (
@@ -9,9 +10,11 @@ export default function Card({ title, imageSource, leftDescription }) {
             </div>
             <div className="text-info">
                 <div className="title">{title}</div>
-                <div className="left-description">{leftDescription}</div>
-                <div className="right-description">
-                    <PrimaryButton className="primary-button" label="Ajouter"/>
+                <div className="left-description">
+                    {leftDescription}
+                    <div className="right-description">
+                        <PrimaryButton className="primary-button" label="Ajouter" />
+                    </div>
                 </div>
             </div>
         </CardStyled>
@@ -19,35 +22,57 @@ export default function Card({ title, imageSource, leftDescription }) {
 }
 
 const CardStyled = styled.div`
-    background: red;
+    background: ${theme.colors.white};
     width: 240px;
     height: 330px;
+    padding-left: 20px;
+    padding-right: 20px;
+    padding-bottom: 10px;
+    display: grid;
+    grid-template-rows: 65% 1fr;
+    align-items: center;
+    box-shadow: -8px 8px 20px 0px rgb(0 0 0 / 20%);
+    border-radius: ${theme.borderRadius.extraRound};
+    
     .image {
-        width: 100px;
-        height: auto;
-        border: 1px solid purple;
+        width: 200px;
+        height: 145px;
+        margin-top: 50px;
+        
         img {
             width: 100%;
             height: 100%;
+            object-fit: contain;
         }
     }
-
+    
     .text-info {
-        border: 1px solid purple;
+        width: 200px;
+        height: 110px;
         
         .title {
-            border: 1px solid purple;
+            font-size: ${theme.fonts.P4};
+            font-family: Amatic SC;
+            font-weight: ${theme.weights.bold};
+            padding: 0px, 5px, 5px, 5px;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            width: 100%;
+            height: 46px;
         }
-
+        
         .left-description {
-            border: 1px solid purple;
-        }
-
-        .right-description {
-            border: 1px solid green;
-
-            .primary-button {
-                border: 1px solid purple;
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            align-items: center;
+            color: ${theme.colors.primary};
+            
+            .right-description {
+                width: 90px;
+                height: 38px;
+                font-weight: ${theme.weights.bold};
+                font-size: ${theme.fonts.XS};
             }
         }
     }
