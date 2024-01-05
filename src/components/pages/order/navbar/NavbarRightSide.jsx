bf8997b19cbc22b0e2f675c55b6741e90afb1be2
Zer0 <Zer0_AbS0luT@proton.me>
2024-01-05 17:43:55 +0100

    ♻️  F06: ORDER > NAVBAR > Add trigger toggle button for mode admin

--- src/components/pages/order/navbar/NavbarRightSide.jsx
+++ src/components/pages/order/navbar/NavbarRightSide.jsx
@@ -1,17 +1,40 @@
 import styled from "styled-components";
 import Profile from "./Profile";
 import ToggleButton from "../../../reusable-ui/ToggleButton"
+import { useState } from "react";
+import ToastAdmin from "./ToastAdmin";
+import { toast } from "react-toastify";
 
 export default function NavbarRightSide({ username }) {
+    const [ isModeAdmin, setIsModeAdmin] = useState(false)
+
+    const displayToastNotification = () => {
+        if (!isModeAdmin) {
+            toast.info("Mode admin activé", {
+                // icon: <FaUserSecret size={30} />,
+                theme: "dark",
+                position: "bottom-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+        setIsModeAdmin(!isModeAdmin)
+    }
     return (
         <NavbarRightSideStyled>
             <div className="admin-button">
                 <ToggleButton 
                     labelIfChecked="DÉSACTIVER LE MODE ADMIN" 
                     labelIfUnchecked="ACTIVER LE MODE ADMIN"
+                    onToggle={displayToastNotification}
                 />
             </div>
-            <Profile username={username}/>
+            <Profile username={username} />
+            <ToastAdmin />
         </NavbarRightSideStyled>
     )
 }
