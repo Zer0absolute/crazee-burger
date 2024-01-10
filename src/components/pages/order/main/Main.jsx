f20ed88e534af0c8d9a01cd7578e97a53884b68a
Zer0 <Zer0_AbS0luT@proton.me>
2024-01-10 16:45:28 +0100

    💄 F07: MAIN > Add layout with Basket

--- src/components/pages/order/main/Main.jsx
+++ src/components/pages/order/main/Main.jsx
@@ -6,25 +6,45 @@ export default function Main() {
     return (
         <MainStyled>
             {/* <div className="basket">basket</div> */}
-            <Menu />
+            <div className="menu-and-admin">
+                <Menu />
+                <div className="admin">
+                    admin
+                </div>
+            </div>
         </MainStyled>
     )
 }
 
 const MainStyled = styled.div`
-    background: ${theme.colors.background_white};
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
-    flex: 1;
-    border-bottom-left-radius: ${theme.borderRadius.extraRound};
-    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+    height: calc(95vh - 10vh);
     display: grid;
     grid-template-columns: 1fr;
-    overflow-y: scroll;
-
     /* This code will be use for Basket */
     /* grid-template-columns: 25% 1fr; */
-    
+    background: ${theme.colors.background_white};
+    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
     /* .basket {
         background: pink;
     } */
+
+    .menu-and-admin {
+        display: grid;
+        position: relative;
+        overflow-y: hidden;
+        border-bottom-left-radius: ${theme.borderRadius.extraRound};
+        border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+        .admin {
+            left: 0;
+            right: 0;
+            bottom: 0;
+            height: 250px;
+            background: red;
+            position: absolute;
+        }
+    }
 `;
--- src/components/pages/order/main/Menu.jsx
+++ src/components/pages/order/main/Menu.jsx
@@ -18,11 +18,12 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
-    background: ${theme.colors.background_white};
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
     grid-row-gap: 60px;
-    padding: 50px 50px 150px;
+    overflow-y: scroll;
     justify-items: center;
+    padding: 50px 50px 150px;
+    background: ${theme.colors.background_white};
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 `;
