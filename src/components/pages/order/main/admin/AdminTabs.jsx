39c1b5fbcf614aefa142a32dd5ed552f3937d0cb
Zer0 <Zer0_AbS0luT@proton.me>
2024-01-10 23:14:18 +0100

    💄 F07: ADMIN > AdminTabs > Tab > Add Tab

--- src/components/pages/order/main/admin/AdminTabs.jsx
+++ src/components/pages/order/main/admin/AdminTabs.jsx
@@ -1,11 +1,17 @@
 import styled from "styled-components";
+import Tab from "./Tab";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 
 export default function AdminTabs() {
+    
     return (
-        <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+        <AdminTabsStyled>
+            <Tab Icon={<FiChevronDown />}/>
+        </AdminTabsStyled>
     )
 }
 
 const AdminTabsStyled = styled.div`
-    border: 1px solid blue;
+    display: flex;
+    padding: 0 20px;
 `;
---
+++ src/components/pages/order/main/admin/Tab.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+
+export default function Tab({ Icon }) {
+    return (
+        <TabStyled>
+            <div className="icon">{Icon}</div>
+        </TabStyled>
+    )
+}
+
+const TabStyled = styled.div`
+    height: 43px;
+    padding: 0 22px;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    cursor: pointer;
+
+    position: relative;
+    left: 5%;
+    top: 1px;
+
+    font-size: ${theme.fonts.P0};
+    color: ${theme.colors.greySemiDark};
+
+    background: ${theme.colors.white};
+    box-shadow: 0px -6px 8px -2px rgba(0, 0, 0, 0.10);    
+
+    border-width: 1px 1px 2px 1px;
+    border-style: solid;
+    border-color: ${theme.colors.greyLight};
+
+    border-radius: ${theme.borderRadius.round};
+    border-bottom-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+
+    &:hover {
+        border-bottom: 2px solid ${theme.colors.white};
+    }
+
+    .icon {
+        display: flex;
+    }
+`;
